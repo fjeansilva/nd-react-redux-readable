3d854c21416660e610acbb2d88ef3f7964b4341a
Jean Silva <jeancarlos.silva@me.com>
2018-01-26 11:06:19 -0200

    feat: Included footer component

---
+++ frontend/src/components/footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="container-fluid footer">
+    <div className="row">
+      <div className="col-12 text-center spacing">
+        <i className="fa fa-optin-monster" aria-hidden="true"></i>
+        <span className="d-block">ND Readable @2018</span>
+      </div>
+    </div>
+  </div>
+);
+
+export default Footer;
