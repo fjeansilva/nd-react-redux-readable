3248e9c194b4a56169f9fac513fd62953e1947e6
Jean Silva <jeancarlos.silva@me.com>
2018-02-09 09:35:58 -0200

    refactor: Removed Order by options to OrderBy component.

--- frontend/src/components/header/MenuActions.js
+++ frontend/src/components/header/MenuActions.js
@@ -1,15 +1,10 @@
 import React from 'react';
+import OrderByContainer from '../orderby/OrderByContainer';
 
 const MenuActions = () => (
   <section className="actions">
     <a href="/" className="actions__add">Novo post</a>
-    <ul className="actions__orderby">
-      <li className="orderby__item">Ordernar por</li>
-      <li className="orderby__item orderby__item--active">
-        <a href="/" className="item item--active">Votos</a>
-      </li>
-      <li className="orderby__item"><a href="/" className="item">Data criação</a></li>
-    </ul>
+    <OrderByContainer />
   </section>
 );
 
