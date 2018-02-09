f0d393316a4c5accde398b4f1477ff5907cb11dc
Jean Silva <jeancarlos.silva@me.com>
2018-02-09 09:28:07 -0200

    refactor: Added news libs and config Redux   - Included react-redux lib.   - Config store.   - Importing reducers and config redux-thunk middleware.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,29 @@
+/* global document, window */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from './reducers';
 import './index.css';
 import App from './components/app';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+const store = createStore(
+  reducer,
+  enhancer,
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'),
+);
+
 registerServiceWorker();
