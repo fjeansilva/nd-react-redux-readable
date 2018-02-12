0f26dc5aa416a41a105510ad4e057172398d3e7e
Jean Silva <jeancarlos.silva@me.com>
2018-02-12 12:06:59 -0200

    refactor: Added score prop

--- frontend/src/utils/VoteScore.js
+++ frontend/src/utils/VoteScore.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const VoteScore = () => (
+const VoteScore = ({ score }) => (
   <div className="vote__score">
     <i className="fa fa-caret-up" aria-hidden="true" />
-    <span className="score">555</span>
+    <span className="score">{score}</span>
     <i className="fa fa-caret-down" aria-hidden="true" />
   </div>
 );
