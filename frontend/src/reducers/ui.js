3ec236cc4846d541d7fb170f5f564a1792d16dcd
Jean Silva <jeancarlos.silva@me.com>
2018-02-13 15:07:10 -0200

    feat: Added ui reducer file

---
+++ frontend/src/reducers/ui.js
@@ -0,0 +1,33 @@
+import {
+  FETCH_POSTS_REQUEST,
+  FETCH_POSTS_RECEIVE,
+} from '../constants/ActionsTypes';
+
+const initialState = {
+  postList: {
+    isFetching: false,
+  },
+};
+
+function uiReducer(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_POSTS_REQUEST:
+      return {
+        ...state,
+        postList: {
+          isFetching: true,
+        },
+      };
+    case FETCH_POSTS_RECEIVE:
+      return {
+        ...state,
+        postList: {
+          isFetching: false,
+        },
+      };
+    default:
+      return state;
+  }
+}
+
+export default uiReducer;
