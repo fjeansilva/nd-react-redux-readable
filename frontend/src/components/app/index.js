49f4a0e0e280ec97bbf3db97703a137b4dd41f91
Jean Silva <jeancarlos.silva@me.com>
2018-02-14 09:55:12 -0200

    refactor: Added routes to the app

--- frontend/src/components/app/index.js
+++ frontend/src/components/app/index.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import Header from '../header';
-import MenuActions from '../header/MenuActions';
+import Menu from '../menu';
 import NavContainer from '../nav/NavContainer';
-import Post from '../post';
+import PostContainer from '../post/PostContainer';
 import Footer from '../footer';
 
 const App = () => (
   <div className="wrapper">
     <Header />
-    <MenuActions />
+    <Menu />
     <NavContainer />
-    <Post />
+    <Route exact path="/" component={PostContainer} />
+    <Route
+      exact
+      path="/:category"
+      render={() => (
+        <h1>List posts of category</h1>
+      )}
+    />
+    <Route
+      exact
+      path="/:category/:post_id"
+      render={() => (
+        <h1>Detalhes do post</h1>
+      )}
+    />
     <Footer />
   </div>
 );
