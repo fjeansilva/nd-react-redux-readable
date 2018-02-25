72a984d1040cc8d82956443e2f0de1cb005493a5
Jean Silva <jeancarlos.silva@me.com>
2018-02-25 10:59:00 -0300

    refactor: Add new properties to VoteScore component Included onUpVote and onDownVote properties.

--- frontend/src/utils/VoteScore.js
+++ frontend/src/utils/VoteScore.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const VoteScore = ({ score }) => (
+const VoteScore = ({ score, onUpVote, onDownVote }) => (
   <div className="vote__score">
-    <i className="fa fa-caret-up" aria-hidden="true" />
+    <i className="fa fa-caret-up" aria-hidden="true" onClick={onUpVote} />
     <span className="score">{score}</span>
-    <i className="fa fa-caret-down" aria-hidden="true" />
+    <i className="fa fa-caret-down" aria-hidden="true" onClick={onDownVote} />
   </div>
 );
+
+VoteScore.propTypes = {
+  score: PropTypes.number.isRequired,
+  onUpVote: PropTypes.func.isRequired,
+  onDownVote: PropTypes.func.isRequired,
+};
 
 export default VoteScore;
