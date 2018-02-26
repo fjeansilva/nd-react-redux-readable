6d35d5c9aa47d0b31982b07ac8a1d07e08e51e33
Jean Silva <jeancarlos.silva@me.com>
2018-02-26 08:03:12 -0300

    feat: Added votePost function

--- frontend/src/components/post/actions.js
+++ frontend/src/components/post/actions.js
@@ -2,14 +2,23 @@ import {
   FETCH_POSTS_REQUEST,
   FETCH_POSTS_RECEIVE,
   FETCH_POSTS_FAILURE,
+  POST_VOTE_SCORE,
 } from '../../constants/ActionsTypes';
-import { getPosts } from '../../utils/ReadableAPI';
+import { getPosts, vote } from '../../utils/ReadableAPI';
 
-export default function fetchPosts(category) {
+export function fetchPosts(category) {
   return (dispatch) => {
     dispatch({ type: FETCH_POSTS_REQUEST });
     return getPosts(category)
       .then(data => dispatch({ type: FETCH_POSTS_RECEIVE, posts: data }))
+      .catch(err => dispatch({ type: FETCH_POSTS_FAILURE, err }));
+  };
+}
+
+export function votePost(id, option) {
+  return (dispatch) => {
+    vote(id, option)
+      .then(data => dispatch({ type: POST_VOTE_SCORE, post: data }))
       .catch(err => dispatch({ type: FETCH_POSTS_FAILURE, err }));
   };
 }
