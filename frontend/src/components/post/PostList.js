cf49249db4b133876bf4c2b23f6d91d92da03b7a
Jean Silva <jeancarlos.silva@me.com>
2018-02-26 08:03:58 -0300

    feat: Added updateVote prop The updateVote func will be passed to the post component.

--- frontend/src/components/post/PostList.js
+++ frontend/src/components/post/PostList.js
@@ -2,14 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Post from './index';
 
-const PostList = ({ items }) => (
+const PostList = ({ items, updateVote }) => (
   <div>
-    {items.map(item => <Post key={item.id} post={item} />)}
+    {items.map(item => <Post key={item.id} post={item} updateVote={updateVote} />)}
   </div>
 );
 
 PostList.propTypes = {
   items: PropTypes.array.isRequired,
+  updateVote: PropTypes.func.isRequired,
 };
 
 export default PostList;
