ae557a6caefd73c2df94dd407acc00b4aff32f4a
Jean Silva <jeancarlos.silva@me.com>
2018-03-06 20:40:20 -0300

    refactor: Included PostInfo and proptypes

--- frontend/src/components/post/index.js
+++ frontend/src/components/post/index.js
@@ -5,28 +5,41 @@ import Category from '../category';
 import PostSummary from './PostSummary';
 import PostTitle from './PostTitle';
 import PostActions from './PostActions';
-import TimeAgo from '../../utils/TimeAgo';
-import UserInfo from '../user';
+import PostInfo from './PostInfo';
 import VoteScore from '../../utils/VoteScore';
 
 const Post = ({ post, updateVote }) => (
   <article className="post">
-    <VoteScore score={post.voteScore} onUpVote={() => updateVote(post.id, 'upVote')} onDownVote={() => updateVote(post.id, 'downVote')} />
+    <VoteScore
+      score={post.voteScore}
+      onUpVote={() => updateVote(post.id, 'upVote')}
+      onDownVote={() => updateVote(post.id, 'downVote')}
+    />
     <PostSummary>
       <Category name={post.category} />
       <Link to={`/${post.category}/${post.id}`}>
         <PostTitle text={post.title} />
       </Link>
-      <TimeAgo date={post.timestamp}>
-        <UserInfo user={post.author} />
-      </TimeAgo>
-      <PostActions commentCount={post.commentCount} />
+      <PostInfo
+        date={post.timestamp}
+        userName={post.author}
+        commentCount={post.commentCount}
+      />
+      <PostActions />
     </PostSummary>
   </article>
 );
 
 Post.propTypes = {
-  post: PropTypes.object.isRequired,
+  post: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    voteScore: PropTypes.number.isRequired,
+    category: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    timestamp: PropTypes.number.isRequired,
+    author: PropTypes.string.isRequired,
+    commentCount: PropTypes.number.isRequired,
+  }).isRequired,
   updateVote: PropTypes.func.isRequired,
 };
 
