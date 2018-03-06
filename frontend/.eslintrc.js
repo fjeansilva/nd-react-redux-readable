6667525276380903724b1faf6a16b218649a23c1
Jean Silva <jeancarlos.silva@me.com>
2018-03-06 20:06:22 -0300

    chore: Added news rules

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
     "extends": "airbnb",
     "rules": {
-        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        "jsx-a11y/anchor-is-valid": [ "error", {
+            "components": [ "Link" ],
+            "specialLink": [ "to" ]
+          }],
+        "import/no-named-as-default": 0
     },
 };
