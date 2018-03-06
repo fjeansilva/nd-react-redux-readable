55efb68d510c7ff8015f4a90cf74945d17cd86e3
Jean Silva <jeancarlos.silva@me.com>
2018-03-06 20:39:10 -0300

    feat: included fetchPost function

--- frontend/src/components/post/actions.js
+++ frontend/src/components/post/actions.js
@@ -1,15 +1,16 @@
 import {
+  GET_POST,
   FETCH_POSTS_REQUEST,
   FETCH_POSTS_RECEIVE,
   FETCH_POSTS_FAILURE,
   POST_VOTE_SCORE,
 } from '../../constants/ActionsTypes';
-import { getPosts, vote } from '../../utils/ReadableAPI';
+import { getPost, getPosts, vote } from '../../utils/ReadableAPI';
 
 export function fetchPosts(category) {
   return (dispatch) => {
     dispatch({ type: FETCH_POSTS_REQUEST });
-    return getPosts(category)
+    getPosts(category)
       .then(data => dispatch({ type: FETCH_POSTS_RECEIVE, posts: data }))
       .catch(err => dispatch({ type: FETCH_POSTS_FAILURE, err }));
   };
@@ -19,6 +20,14 @@ export function votePost(id, option) {
   return (dispatch) => {
     vote(id, option)
       .then(data => dispatch({ type: POST_VOTE_SCORE, post: data }))
+      .catch(err => dispatch({ type: FETCH_POSTS_FAILURE, err }));
+  };
+}
+
+export function fetchPost(id) {
+  return (dispatch) => {
+    getPost(id)
+      .then(data => dispatch({ type: GET_POST, post: data }))
       .catch(err => dispatch({ type: FETCH_POSTS_FAILURE, err }));
   };
 }
