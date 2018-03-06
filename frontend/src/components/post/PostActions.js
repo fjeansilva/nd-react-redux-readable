342457a456a333caf68935b15e6678cf4a8fc7a1
Jean Silva <jeancarlos.silva@me.com>
2018-03-06 20:17:49 -0300

    refactor: Removed comments link

--- frontend/src/components/post/PostActions.js
+++ frontend/src/components/post/PostActions.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import Icon from '../../utils/Icon';
 
-const PostActions = ({ commentCount }) => (
+const PostActions = () => (
   <div className="post__actions">
-    <a href="/">
-      <Icon name="fa-comments-o" />
-      <span> {commentCount} comment(s)</span>
-    </a>
     <a href="/">edit</a>
     <a href="/">delete</a>
   </div>
