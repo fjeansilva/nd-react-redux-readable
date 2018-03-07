46d93664dd946f17310a3976491e018fc1618f3e
Jean Silva <jeancarlos.silva@me.com>
2018-03-06 21:49:41 -0300

    feat: Added PostInfo component

---
+++ frontend/src/components/post/PostInfo.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TimeAgo from 'timeago-react';
+import UserInfo from '../UserInfo';
+import CommentInfo from '../comment/CommentInfo';
+
+const PostInfo = ({ date, userName, commentCount }) => (
+  <p>
+    Submmited
+    <TimeAgo
+      datetime={date}
+      live
+    /> by <UserInfo user={userName} /> - <CommentInfo commentCount={commentCount} />
+  </p>
+);
+
+PostInfo.propTypes = {
+  date: PropTypes.number.isRequired,
+  userName: PropTypes.string.isRequired,
+  commentCount: PropTypes.number.isRequired,
+};
+
+export default PostInfo;
