3ab992a00541a440d18f44beb2aef32738a38e04
Jean Silva <jeancarlos.silva@me.com>
2018-03-06 21:44:13 -0300

    feat: Added selectedPost state

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,12 +1,13 @@
 import { combineReducers } from 'redux';
 import categories from '../components/category/reducers';
 import postsOrderBy from '../components/orderby/reducers';
-import posts from '../components/post/reducers';
+import { posts, selectedPost } from '../components/post/reducers';
 import ui from './ui';
 
 export default combineReducers({
   categories,
   postsOrderBy,
+  selectedPost,
   posts,
   ui,
 });
