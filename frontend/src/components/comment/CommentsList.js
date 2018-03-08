b457ea87bf26536fd83b77229cad81045e03c19b
Jean Silva <jeancarlos.silva@me.com>
2018-03-08 19:50:44 -0300

    refactor: Included props, Comment component and proptypes

--- frontend/src/components/comment/CommentsList.js
+++ frontend/src/components/comment/CommentsList.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Comment from './index';
 
-const CommentsList = () => (
+const CommentsList = ({ comments }) => (
   <section className="comments">
-    <h1>CommentList</h1>
+    <h4 className="comments__title">{comments.length} Comment(s)</h4>
+    {comments.filter(c => !c.parentDeleted).map(c => (
+      <Comment
+        key={c.id}
+        id={c.id}
+        timestamp={c.timestamp}
+        author={c.author}
+        voteScore={c.voteScore}
+        body={c.body}
+      />
+    ))}
   </section>
 );
+
+CommentsList.propTypes = {
+  comments: PropTypes.array.isRequired,
+};
 
 export default CommentsList;
