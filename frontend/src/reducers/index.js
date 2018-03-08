57bd37ce4ff3d1c278d5cf272d700706a5d0dbf5
Jean Silva <jeancarlos.silva@me.com>
2018-03-08 20:00:46 -0300

    feat: Added comments reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import categories from '../components/category/reducers';
 import postsOrderBy from '../components/orderby/reducers';
+import comments from '../components/comment/reducers';
 import { posts, selectedPost } from '../components/post/reducers';
 import ui from './ui';
 
@@ -9,5 +10,6 @@ export default combineReducers({
   postsOrderBy,
   selectedPost,
   posts,
+  comments,
   ui,
 });
