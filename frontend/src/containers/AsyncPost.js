3be0ed9e6e089e66724b86f83776b50b80b068b5
Jean Silva <jeancarlos.silva@me.com>
2018-03-08 19:59:49 -0300

    refactor: Included comments and fetch comments props

--- frontend/src/containers/AsyncPost.js
+++ frontend/src/containers/AsyncPost.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { fetchPost, votePost } from '../components/post/actions';
+import { fetchComments } from '../components/comment/actions';
 import PostDetails from '../components/post/PostDetails';
 
 class AsyncPost extends Component {
@@ -10,6 +11,7 @@ class AsyncPost extends Component {
     const { params } = match;
     if (params.post_id) {
       this.props.fetchPost(params.post_id);
+      this.props.fetchComments(params.post_id);
     }
   }
 
@@ -18,25 +20,32 @@ class AsyncPost extends Component {
   }
 
   render() {
-    const { post } = this.props;
+    const { post, comments } = this.props;
     if (!post.id) return 'Loading...';
     return (
-      <PostDetails post={this.props.post} updateVote={this.handleUpdateVote} />
+      <PostDetails
+        post={this.props.post}
+        updateVote={this.handleUpdateVote}
+        comments={comments}
+      />
     );
   }
 }
 
 const mapStateToProps = state => ({
   post: state.selectedPost,
+  comments: state.comments.byId ? Object.values(state.comments.byId) : [],
 });
 
 const mapDispatchToProps = dispatch => ({
   fetchPost: id => dispatch(fetchPost(id)),
+  fetchComments: id => dispatch(fetchComments(id)),
   votePost: (id, vote) => dispatch(votePost(id, vote)),
 });
 
 AsyncPost.propTypes = {
   fetchPost: PropTypes.func.isRequired,
+  fetchComments: PropTypes.func.isRequired,
   match: PropTypes.shape({
     params: PropTypes.shape({
       post_id: PropTypes.string.isRequired,
